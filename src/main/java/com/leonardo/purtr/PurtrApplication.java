bfb9d8a78098c60bcc3982cf45ff9289e29b3e47
Leonardo Oliveira <leoxsouza286@gmail.com>
2023-10-14 15:56:11 -0300

    feat: initial setup

---
+++ src/main/java/com/leonardo/purtr/PurtrApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.purtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/purtr/PurtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.purtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
